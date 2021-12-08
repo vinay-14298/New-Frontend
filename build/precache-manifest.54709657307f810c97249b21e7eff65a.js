3b3c9f9538223f1aef4f4f8dae235d9ffce5ff62
root <vtu14298@veltech.edu.in>
2021-12-08 04:45:07 +0000

    first commit

---
+++ build/precache-manifest.54709657307f810c97249b21e7eff65a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fd2a0d3dbd25ce8ae1c153dee3bddce",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bb577a9aba210a01a36d",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "89a678da5a53c2437d6b",
+    "url": "/static/css/main.9b05a1ca.chunk.css"
+  },
+  {
+    "revision": "bb577a9aba210a01a36d",
+    "url": "/static/js/2.cdac6e34.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.cdac6e34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89a678da5a53c2437d6b",
+    "url": "/static/js/main.6890389b.chunk.js"
+  },
+  {
+    "revision": "1af47c5e440203105a31",
+    "url": "/static/js/runtime-main.66ee3d9b.js"
+  }
+]);
